8f975b09a7f05aa507d35c0a22734b63d087a390
suresh <suresh123@gmail.com>
2025-09-22 19:23:32 +0530

    Arrays

---
+++ Arrays/Array.java
@@ -0,0 +1,14 @@
+public class Array {
+    public static void main(String[] args) {
+        int arr [] = {1,2,3,4,5,6,7};
+
+      System.out.println(arr[0]);
+      for(int i=0; i<arr.length; i++){
+         System.out.print(arr[i] + " ");
+
+      }
+      System.out.println("\narrys length : " + arr.length);
+
+    }
+    
+}
