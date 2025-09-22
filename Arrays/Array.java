5d5e0fe861fe91855581bec0554f5cc7885436b4
suresh <suresh123@gmail.com>
2025-09-22 19:30:03 +0530

    arrays

--- Arrays/Array.java
+++ Arrays/Array.java
@@ -1,14 +1,19 @@
 public class Array {
     public static void main(String[] args) {
-        int arr [] = {1,2,3,4,5,6,7};
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+        String str [] = {"java", "python","js","react", "html"};
+        System.out.println(arr[0]);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
 
-      System.out.println(arr[0]);
-      for(int i=0; i<arr.length; i++){
-         System.out.print(arr[i] + " ");
-
-      }
-      System.out.println("\narrys length : " + arr.length);
+        }
+        System.out.println("\narrys length : " + arr.length);
+      // str used to the enhanced for loop 
+      System.out.println("----------------------------------------------");
+     for(String x : str) {
+        System.out.println(x);
+     }
 
     }
-    
+
 }
