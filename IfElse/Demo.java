7f168da03dca22659304c4f64c9c07faccf74443
suresh <suresh123@gmail.com>
2025-10-01 09:51:49 +0530

    if-else

---
+++ IfElse/Demo.java
@@ -0,0 +1,18 @@
+public class Demo {
+    public static void main(String[] args) {
+        int age =25;
+
+        // If - else decision making statements
+        if(age > 18 ) {
+            System.out.println("elisibile vote");
+        }else{
+            System.out.println("not elisible for vote");
+        }
+
+        if (age <18) {
+            System.out.println("not e");
+        }else{
+            System.out.println("e vote");
+        }
+    }  
+}
