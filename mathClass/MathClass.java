a808407310023695ae9a576ec3a21bd09c169023
suresh <suresh123@gmail.com>
2025-10-04 17:27:24 +0530

    rounded + abs + signum

--- mathClass/MathClass.java
+++ mathClass/MathClass.java
@@ -1,7 +1,17 @@
 public class MathClass {
     public static void main(String[] args) {
+        // mathametical function and contaits
+        // without create a object becaues of all method is static
         double a = 12.85;
-        System.out.println(Math.round(a));
+        System.out.println("round  :" + Math.round(a)); // return the int
+        System.out.println("floor :" + Math.floor(a)); // not cosider the decimal value
+        System.out.println("ceil :" + Math.ceil(a)); // return the double
+
+        // absolue & sign
+        int x = 0;
+        System.out.println("absolute : " + Math.abs(x)); // converted the negative to positive value
+        System.out.println("signma : " + Math.signum(x));
+
     }
 
 }
