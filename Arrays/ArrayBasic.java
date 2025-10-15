3ba1391ee1b33680ae6039742dcac152a42d32cf
suresh <suresh123@gmail.com>
2025-10-15 12:42:58 +0530

    arrays basic

---
+++ Arrays/ArrayBasic.java
@@ -0,0 +1,19 @@
+public class ArrayBasic {
+    public static void main(String[] args) {
+        // array intialization
+        int arr[] = new int[4];
+        // int which type store the element you can use it = ex char and float etc ..
+        // updatation
+        arr[0] = 2;
+        arr[1] = 3;
+        arr[2] = 4;
+        arr[3] = 5;
+
+        // print
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+    }
+
+}
